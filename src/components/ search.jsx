23e916579fa87ff87a9906e9cd70e867a410ca83
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-04-01 23:41:05 +0800

    Header Design

---
+++ src/components/ search.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const person = {
+    name : "Bruce Wayne",
+    age : 35,
+    location : "Gotham City"
+}
+const Search = ({searchTerm, setSearchTerm}) => {
+    return (
+        <div className="search">
+            <div>
+                <img src="search.svg" alt="search icon" />
+                <input type="text"
+                       placeholder="Search through thousands of movies..."
+                       value={searchTerm}
+                       onChange={(e) => setSearchTerm(e.target.value)}/>
+            </div>
+        </div>
+    )
+}
+export default Search
+ 
