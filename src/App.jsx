f29e0b2dbf5aa45638f1f54e301c4d8c0d974078
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-04-02 21:02:23 +0800

    Movie Card Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
-import Search from "./components/search.jsx";
-import Spinner from "./components/spinner.jsx";
+import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -71,7 +72,7 @@ const App = () => {
                     ) : (
                         <ul>
                             {movieList.map((movie) => (
-                                <p key={movie.id} className="text-white">{movie.title}</p>
+                                <MovieCard key={movie.id} movie={movie}/>
                             ))}
                         </ul>
                     )}
